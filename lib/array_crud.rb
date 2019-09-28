1cbe601ff87b89942155c705579114f574df5788
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-09-28 02:40:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = ["Lluis","Alvy","Ixax","Esma"]
-  my_array << "Eli"
+  eli = "Eli"
+  my_array.push(eli)
 end
 
 def add_element_to_start_of_array(array, element)
